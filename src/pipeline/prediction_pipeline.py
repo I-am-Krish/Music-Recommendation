ed034b50cfb332027b07d50bad46cefc1ce44b4a
Krishnashis Das <81500145+I-am-Krish@users.noreply.github.com>
2024-04-19 20:42:05 +0530

    Commit 5.py

--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -16,21 +16,19 @@ class MusicRecommendationModel:
         self.config = MusicRecommendationModelConfig()
         self.preprocessor = None
         self.model = None
-        self.df = None  # Instance variable for the DataFrame
+        self.df = None  
 
     def load_pretrained_model(self):
         self.preprocessor = load_function(file_path=self.config.preprocessor_path)
         self.model = load_function(file_path=self.config.model_path)
         
-        # Assuming you have a path to your CSV data
         data_path = r'C:\Users\krish\OneDrive\Desktop\Music Recomendation\notebooks\data\music.csv'
         df = pd.read_csv(data_path)
         df = df.iloc[:100001]
         
-        # Add an 'ID' column with sequential numbering starting from 1
         df['ID'] = df.reset_index().index + 1
         
-        self.df = df  # Initialize self.df with the loaded DataFrame
+        self.df = df 
 
     def find_song_index(self, song_name, data):
         try:
@@ -44,16 +42,13 @@ class MusicRecommendationModel:
         if self.df is None:
             raise CustomException("DataFrame is not initialized.")
         
-        # Reset index to create the 'ID' column
         self.df.reset_index(drop=True, inplace=True)
         
         song_features = self.df[self.df['name'] == song_name].drop('name', axis=1)
         song_features_preprocessed = self.preprocessor.transform(song_features)
         
-        # Find the nearest neighbors
         distances, indices = self.model.kneighbors(song_features_preprocessed)
         
-        # Get the song names of the neighbors
         recommended_songs = self.df.iloc[indices[0]]['name'].values
         
         return recommended_songs
